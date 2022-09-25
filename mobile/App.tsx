4177430c576875ab8ddceadd5124f877342969a3
Hiago Carvalho <hiaagocarvalho@gmail.com>
2022-09-25 03:42:33 -0300

    In progress

---
+++ mobile/App.tsx
@@ -0,0 +1,43 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>
+        HAHA
+      </Text>
+
+      <Button title="Send 1" />
+      <Button title="Send 2" />
+      <Button title="Send 3" />
+      <Button title="te amo linda" />
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+});
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.tsx',
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/bg-galaxy.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 27.08%, #43E7AD 33.94%, #E1D55D 48.57%)',
+      },
+    },
+  },
+  plugins: [],
+}
